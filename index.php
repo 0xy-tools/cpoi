24fbb01ac31cc2d90ba35de6627b4cb8a9fe37f8
MagicTINTIN <magictintin@proton.me>
2024-09-14 17:06:19 +0200

    add unique command

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ include_once("includes/db.php");
 include_once("includes/utils.php");
 $db = dbConnect();
 
-// create clipboard
-if (isset($_GET["c"]) && strlen(htmlspecialchars($_GET["c"])) < 1800) {
+
+// CREATE CLIPBOARD \\
+
+function createClipboard(string $content, string $type = ""): void
+{
+    global $db;
+
     $codeGen = false;
     $codeVal = "";
     while (!$codeGen) {
@@ -21,21 +26,55 @@ if (isset($_GET["c"]) && strlen(htmlspecialchars($_GET["c"])) < 1800) {
             $codeGen = true;
     }
 
-    $sqlQuery = 'INSERT INTO cpoi(code, value) VALUES (:code, :value)';
+    $sqlQuery = 'INSERT INTO cpoi(type, code, value) VALUES (:type, :code, :value)';
 
     $insertCPoi = $db->prepare($sqlQuery);
     $insertCPoi->execute([
-        'value' => htmlspecialchars($_GET["c"]),
+        'type' => $type,
+        'value' => $content,
         'code' => $codeVal
     ]);
     echo $codeVal;
     exit;
 }
 
-// paste clipboard
-if (isset($_GET["p"]) && strlen(htmlspecialchars($_GET["p"])) <= 10 && strlen(htmlspecialchars($_GET["p"])) >= 4) {
+// create normal clipboard
+if (isset($_GET["c"]) && strlen(htmlspecialchars($_GET["c"])) < 1800) {
+    createClipboard(htmlspecialchars($_GET["c"]));
 }
 
-// delete clipboard
+// create unique normal clipboard
+if (isset($_GET["uc"]) && strlen(htmlspecialchars($_GET["uc"])) < 1800) {
+    createClipboard(htmlspecialchars($_GET["uc"]), "u");
+}
+
+
+// PASTE CLIPBOARD \\
+
+if (isset($_GET["p"]) && strlen(htmlspecialchars($_GET["p"])) <= 10 && strlen(htmlspecialchars($_GET["p"])) >= 4) {
+    $cpoiStatement = $db->prepare('SELECT * FROM cpoi WHERE code = :code');
+    $cpoiStatement->execute([
+        'code' => htmlspecialchars($_GET["p"])
+    ]);
+
+    $codes = $cpoiStatement->fetchAll();
+    if (sizeof($codes) == 0)
+        echo "CPOI ERROR: " . htmlspecialchars($_GET["p"]) . " is not a valid clipboard!";
+    else {
+        
+    }
+}
+
+
+// DELETE CLIPBOARD \\
+
 if (isset($_GET["d"]) && strlen(htmlspecialchars($_GET["d"])) <= 10 && strlen(htmlspecialchars($_GET["d"])) >= 4) {
+    $cpoiStatement = $db->prepare('DELETE FROM cpoi WHERE code = :code');
+    $cpoiStatement->execute([
+        'code' => htmlspecialchars($_GET["d"])
+    ]);
+
+    $codes = $cpoiStatement->fetchAll();
+    if (sizeof($codes) == 0)
+        echo "CPOI ERROR: " . htmlspecialchars($_GET["d"]) . " is not a valid clipboard!";
 }
